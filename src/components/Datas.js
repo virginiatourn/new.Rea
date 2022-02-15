cbc4ea10147398eb2951ab9ee97b6f8cb9f3c545
virginiatourn <82557870+virginiatourn@users.noreply.github.com>
2022-02-15 04:10:01 -0300

    Create Datas.js

---
+++ src/components/Datas.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect, createContext} from "react;"
+import Data from './Data.js';
+
+
+export const DataContext = createContext();
+
+export const DataProvider = (props) =>{ 
+ const [productos, setProductos] = useState([])
+ 
+ useEffect(() => {
+   const producto = Data
+   setProductos(producto)
+   
+   
+ },[])
+    const value = {
+      productos: [productos]
+    }
+  
+    return(
+    <DataContext.Provider value = {value}>
+      {props.children}
+    </DataContext.Provider>
+      )
+ }
