7737f13b417d52e24f89b4d3127d2b585fa8f8a6
virginiatourn <82557870+virginiatourn@users.noreply.github.com>
2022-02-15 04:11:01 -0300

    Create Index.js

---
+++ src/components/Index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Portada} from "./components/images/inicio.jpg";
+import {Link} from "react-router-dom";
+
+export const Inicio = () => {
+return (
+  <div className="inicio">
+    <Link to="/" >
+    <h1>Inicio</h1>
+    </Link>
+  <Link to="/productos" >
+    <h1>Productos</h1>
+  </Link>
+  <img src={Portada} alt="inicio"/>
+</div>
+  )
+}
